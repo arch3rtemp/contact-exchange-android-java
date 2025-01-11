a8e489c1ad4cab0402846cd7dcd74c10644348d8
Archil Asanishvili <arch3r_temp@yahoo.com>
2025-01-11 21:03:51 +0400

    Fix minor compilation error of not resolving entity tables in CardDao

--- app/src/main/java/dev/arch3rtemp/contactexchange/data/db/AppDatabase.java
+++ app/src/main/java/dev/arch3rtemp/contactexchange/data/db/AppDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase;
 
 import dev.arch3rtemp.contactexchange.data.model.CardEntity;
 
-@Database(entities = CardEntity.class, version = 1, exportSchema = false)
+@Database(entities = {CardEntity.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CardDao cardDao();
 }
