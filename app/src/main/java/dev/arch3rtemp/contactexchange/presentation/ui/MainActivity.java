3fa608ba326c5414e063c871afed04434a2519a1
Archil Asanishvili <arch3r_temp@yahoo.com>
2025-02-03 00:10:37 +0400

    * Upgrade targetSdk up to 15 * Implement edge to edge

--- app/src/main/java/dev/arch3rtemp/contactexchange/presentation/ui/MainActivity.java
+++ app/src/main/java/dev/arch3rtemp/contactexchange/presentation/ui/MainActivity.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Toast;
 
+import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
 import androidx.core.os.OperationCanceledException;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -40,6 +44,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            getWindow().setNavigationBarContrastEnforced(false);
+        }
         setContentView(binding.getRoot());
 
         initNavigationWithToolbar();
